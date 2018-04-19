bd05b3589d291f67d4796239a430b9df23d20421
Sirish Kanukunta <sirishkanukunta@Sirishs-MacBook-Pro.local>
2018-04-19 13:35:08 -0400

    server.js

--- server.js
+++ server.js
@@ -1,18 +1,22 @@
-var express = require('express');
+var express = require("express");
 var app = express();
 var bodyParser = require('body-parser');
+var path = require("path");
 
 var PORT = process.env.PORT || 8080;
 
 var jsonParser = bodyParser.json()
 
-var urlencodedParser = bodyParser.urlencoded({ extended: false})
+app.use(bodyParser.urlencoded({ extended: true}));
 
 app.use(bodyParser.json({ Type: 'application/** Json'}))
 
 app.use(bodyParser.raw({ Type: 'application/vnd.custom-type'}))
 
 app.use(bodyParser.text({ Type: 'text/html'}))
+
+require("./routing/apiRoutes.js")(app);
+require("./routing/htmlRoutes.js")(app);
 
 app.listen(PORT, function() {
     console.log("App listening on Port: " + PORT);
